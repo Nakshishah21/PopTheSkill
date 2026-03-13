197392495eec3a0d17ba0455cba945a12e87140d
khushi5125 <patelkhushim512@gmail.com>
2026-03-13 17:40:14 +0530

    Delete Frontend directory

--- Frontend/src/util/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  const userInfo = localStorage.getItem("userInfo");
-  return userInfo ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoutes;
